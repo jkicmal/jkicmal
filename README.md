ca312f628633e13de60882696adbde0d2e52cff5
jkicmal <jakub.kicmal@gmail.com>
2020-05-11 15:11:50 +0200

    new website

--- README.md
+++
@@ -1,1 +0,0 @@
-# Website about me :D
---
+++ precache-manifest.9da7dd1c8b2fb3b4e834ba222ea152e4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7d66deb78538fc3711f777d0b0a3b475",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ca5ef5f8c3c95c81edcf",
+    "url": "/static/css/main.bbe37c8b.chunk.css"
+  },
+  {
+    "revision": "a56ee4325d2319c4080e",
+    "url": "/static/js/2.4a154199.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.4a154199.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ca5ef5f8c3c95c81edcf",
+    "url": "/static/js/main.5f7416bb.chunk.js"
+  },
+  {
+    "revision": "d40fa7f62f6be28cf04b",
+    "url": "/static/js/runtime-main.7ddf7ca5.js"
+  },
+  {
+    "revision": "88404a0f5998cef73ef4ed9d980256dd",
+    "url": "/static/media/icons8-react-160.88404a0f.png"
+  }
+]);
